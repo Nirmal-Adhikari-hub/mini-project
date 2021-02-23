a7db6e7ec1fdc53d0239bcd7a3eebe14d880de3c
Nirmal Adhikari <nirmaladhikari@na.local>
2021-02-23 17:54:25 +0545

    1st push

---
+++ Blink_copy.ino
@@ -0,0 +1,172 @@
+#include <ESP8266WiFi.h>
+
+// Replace with your network credentials
+const char* ssid     = "santosh9_fpkhr";
+const char* password = "9816100105";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+// defines pins numbers
+
+const int trigPin = 5;  //D1
+const int echoPin = 4;  //D2
+
+//thermistor variables
+const double VCC = 3.3;             // NodeMCU on board 3.3v vcc
+const double R2 = 10000;            // 10k ohm series resistor
+const double adc_resolution = 1023; // 10-bit adc
+
+const double A = 0.001129148;   // thermistor equation parameters
+const double B = 0.000234125;
+const double C = 0.0000000876741; 
+
+
+// defines variables
+long duration;
+int distance;
+
+int decimalPrecision = 2;
+
+void setup() {
+  Serial.begin(9600);
+ 
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+
+  double Vout, Rth, temperature, adc_value; 
+
+  adc_value = analogRead(A0);
+  Vout = (adc_value * VCC) / adc_resolution;
+  Rth = (VCC * R2 / Vout) - R2;
+
+/*  Steinhart-Hart Thermistor Equation:
+ *  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)
+ *  where A = 0.001129148, B = 0.000234125 and C = 8.76741*10^-8  */
+  temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)),3))));   // Temperature in kelvin
+
+  temperature = temperature - 273.15;  // Temperature in degree celsius
+  Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" degree celsius");
+  delay(500);
+
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+delay(2000);
+  
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {  
+    
+    // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            client.println("<p>Distance:");
+            client.println(distance); 
+            client.println("</p>");
+            client.println("<p>Temerature:");
+            client.println(temperature);
+            client.print("degree Celsius"); 
+            client.println("</p>");
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } 
+          else 
+          { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r')
+         {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+         }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
